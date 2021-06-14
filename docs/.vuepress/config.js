205d27d65301c321f9eaccf4ce129075a0a84a66
ZHUbinxin0207 <snoopy_zuozuo@qq.com>
2021-06-14 17:06:17 +0800

    修改icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,17 @@ module.exports = {
   dest: "./dist",
   themeConfig: {
     logo: "/logo.png",
-    head: [["link", { rel: "icon", href: "/logo.png" }]],
+    head: [
+      [
+        "link",
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "https://cn.vuejs.org/images/icons/favicon-32x32.png",
+        },
+      ],
+    ],
     sidebar: "auto",
   },
 };
