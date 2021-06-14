0fda3219bb867635b5c8dbc5ed32042275d767df
ZHUbinxin0207 <snoopy_zuozuo@qq.com>
2021-06-14 16:25:02 +0800

    优化目录结构，增加icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  title: 'lds-app-cli',
-  description: '前端脚手架,多页面应用',
-  dest: "./docs/.vuepress/dist",
+  title: "lds-app-cli",
+  description: "前端脚手架,多页面应用",
+  dest: "./dist",
   themeConfig: {
-    sidebar: 'auto'
-  }
-}
+    logo: "/imgs/avatar.jpg",
+    head: [["link", { rel: "icon", href: "/hero.png" }]],
+    sidebar: "auto",
+  },
+};
